1173f04a208e34c3e3badc1cc4e0404339e25324
Zechw <zechwilliams@gmail.com>
2018-05-08 21:59:47 -0600

    initial commit

---
+++ rythmRunner.js
@@ -0,0 +1,37 @@
+let rythmRunner = new function() {
+  //KeyCodes 81, 87, 79, 80 | q, w, o, p
+  this.keyRotation = [81, 87, 79, 80];
+  this.currentKeyIndex = 0;
+  this.currentKey = this.keyRotation[this.currentKeyIndex];
+  this.pressKey = function(keyCode) {
+    let e = new Event('keydown');
+    e.keyCode = keyCode;
+    document.dispatchEvent(e);
+  }
+  this.releaseKey = function(keyCode) {
+    let e = new Event('keyup');
+    e.keyCode = keyCode;
+    document.dispatchEvent(e);
+  }
+  this.switchKeys = function(){
+    let nextIndex = this.currentKeyIndex + 1;
+    if (nextIndex >= this.keyRotation.length) {
+      nextIndex = 0;
+    }
+    this.releaseKey(this.currentKey);
+    this.pressKey(this.keyRotation[nextIndex]);
+    this.currentKeyIndex = nextIndex;
+  }
+  this.run = function(){
+    this.pressKey(this.currentKeyIndex)
+    this.switchInterval = setInterval(() => {
+      this.switchKeys();
+    }, 2000);
+  }
+  this.stopRun = function(){
+    clearInterval(this.switchInterval);
+    this.keyRotation.forEach((key) => {
+      this.releaseKey(key);
+    })
+  }
+}
